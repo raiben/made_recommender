fc5d7cf4290cd2d09c5d3873c759d9d443fe25fc
Rubén Héctor García <raiben@gmail.com>
2019-05-11 12:57:56 +0200

    Extended diagram

--- papers/helpers.py
+++ papers/helpers.py
@@ -19,6 +19,47 @@ def reverse_dictionary(original_dictionary):
     return reversed_dictionary
 
 
+import pygraphviz as pgv
+import os
+def draw_graphviz(dot, filename):
+    new_dot = dot.replace('type="database"', 'shape="cylinder" margin="0.2"')
+    new_dot = new_dot.replace('type="process"', 'shape="box" margin="0.2" style="filled" fillcolor="gray91"')
+    new_dot = new_dot.replace('type="data"', 'shape="parallelogram" margin="0"')
+
+    G = pgv.AGraph(string=new_dot)
+    G.layout(prog='dot')
+    if not os.path.isdir('figures'):
+        os.makedirs('figures')
+    G.draw(os.path.join('figures', filename))
+
+
 if __name__ == '__main__':
-    content = read_compressed_json('../datasets/scrapper/cache/20190501/films_tropes_20190501.json.bz2')
+    content = read_compressed_json('../datasets/scraper/cache/20190501/films_tropes_20190501.json.bz2')
     reversed_content = reverse_dictionary(content)
+
+    workflow = '''
+    digraph {
+        tvtropes[label="TVTropes\nwebsite" type="database"];
+        scraper[label="Scraper\nprocess" type="process"];
+        dataset1[label="Dataset\nFilm->(tropes)" type="data"];
+        imdb[label="IMDB\ndatabases" type="database"];
+        matcher[label="Film\nmatcher\nprocess" type="process"];
+        dataset2[label="Dataset\nFilm->(tropes,\nrating, genres)" type="data"];
+        recommender[label="Trope\nRecommender\nprocess" type="process"];
+        user[label="User's\npre-selected\ntropes" type="data"];
+        builder[label="Trope\nbuilder\nprocess" type="process"];
+        trope_sequence[label="Optimal\ntrope\nsequence" type="data"]
+
+        tvtropes -> scraper;
+        scraper -> dataset1;
+        dataset1 -> matcher;
+        imdb -> matcher;
+        matcher -> dataset2;
+        dataset2 -> recommender;
+        recommender -> builder
+        user -> builder
+        builder -> trope_sequence
+    }'''
+
+    draw_graphviz(workflow, "main_workflow2.pdf")
+
