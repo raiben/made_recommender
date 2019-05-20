95f0504634d43389609910c1289bb1789d7aba18
Rubén Héctor García <raiben@gmail.com>
2019-05-20 21:09:20 +0200

    reduce ambiguity

--- mapper/film_mapper.py
+++ mapper/film_mapper.py
@@ -48,13 +48,14 @@ class FilmMapper(BaseScript):
     EXCLUDED_ENTRY_TYPES = ['tvEpisode', 'tvSeries', 'tvSpecial', 'tvShort', 'videoGame', 'tvMiniSeries',
                             'titleType']
     INCLUDE_FILMS_FROM_IMDB_WITHOUT_YEAR = False
-    POPULARITY_FACTOR_TO_FILTER_ALTERNATIVES = 3
+    POPULARITY_FACTOR_TO_FILTER_ALTERNATIVES = 2
 
     def __init__(self, tvtropes_films_file, imdb_titles_file, imdb_ratings_file, target_dataset):
         parameters = dict(tvtropes_films_file_name=tvtropes_films_file, imdb_titles_file=imdb_titles_file,
                           imdb_ratings_file=imdb_ratings_file, target_dataset=target_dataset,
                           excluded_entry_types=self.EXCLUDED_ENTRY_TYPES,
-                          include_films_from_imdb_without_year=self.INCLUDE_FILMS_FROM_IMDB_WITHOUT_YEAR)
+                          include_films_from_imdb_without_year=self.INCLUDE_FILMS_FROM_IMDB_WITHOUT_YEAR,
+                          popularity_factor=self.POPULARITY_FACTOR_TO_FILTER_ALTERNATIVES)
 
         BaseScript.__init__(self, parameters)
 
@@ -83,7 +84,7 @@ class FilmMapper(BaseScript):
         types = set()
 
         self._info(f'Loading basic information from IMDb dataset: {self.imdb_titles_file}')
-        with open(self.imdb_titles_file, 'r') as films_imdb_titles:
+        with open(self.imdb_titles_file, 'r', encoding='utf-8') as films_imdb_titles:
             imdb_lines_counter = 0
             for line in films_imdb_titles:
                 try:
@@ -162,6 +163,13 @@ class FilmMapper(BaseScript):
                 if name in self.films_in_imdb_by_name:
                     self.tvtropes_imdb_map[film_name_tvtropes].extend(self.films_in_imdb_by_name[name])
 
+        self._add_to_summary('Films matched times: 0', len(self._matches_equal(0)))
+        self._add_to_summary('Films matched times: 1', len(self._matches_equal(1)))
+        self._add_to_summary('Films matched times: 2', len(self._matches_equal(2)))
+        self._add_to_summary('Films matched times: 3', len(self._matches_equal(3)))
+        self._add_to_summary('Films matched times: 4+', len(self._matches_equal_or_higher(4)))
+
+        self._info(f'Reducing ambiguities by selecting films with more popularity')
         self.tvtropes_imdb_map_popularity = {}
         for key in self.tvtropes_imdb_map:
             self.tvtropes_imdb_map_popularity[key] = sorted(self.tvtropes_imdb_map[key], key=lambda x: x.votes,
@@ -170,14 +178,23 @@ class FilmMapper(BaseScript):
                 best_popularity = self.tvtropes_imdb_map_popularity[key][0].votes
                 second_best_popularity = self.tvtropes_imdb_map_popularity[key][1].votes
                 popularity_factor = self.safe_division(best_popularity, second_best_popularity)
-                if popularity_factor >= self.POPULARITY_FACTOR_TO_FILTER_ALTERNATIVES:
-                    self.tvtropes_imdb_map_popularity[key] = self.tvtropes_imdb_map_popularity[key][0:1]
 
-        self._add_to_summary('Films matched times: 0', len(self._matches_equal(0)))
-        self._add_to_summary('Films matched times: 1', len(self._matches_equal(1)))
-        self._add_to_summary('Films matched times: 2', len(self._matches_equal(2)))
-        self._add_to_summary('Films matched times: 3', len(self._matches_equal(3)))
-        self._add_to_summary('Films matched times: 4+', len(self._matches_equal_or_higher(4)))
+                #best_candidate = self.tvtropes_imdb_map_popularity[key][0]
+                #second_best_candidate = self.tvtropes_imdb_map_popularity[key][1]
+                #self._info(f'Film {key}: best candidate {best_candidate.title} ({best_candidate.id}, '
+                #           f'{best_candidate.start_year}) is '
+                #           f'{popularity_factor} times more popular than the second candidate '
+                #           f'{second_best_candidate.title} ({second_best_candidate.id}, '
+                #           f'{second_best_candidate.start_year})')
+
+                if popularity_factor >= self.POPULARITY_FACTOR_TO_FILTER_ALTERNATIVES:
+
+                    self._info(f'Film https://tvtropes.org/pmwiki/pmwiki.php/Film/{key}')
+                    for index, film in enumerate(self.tvtropes_imdb_map_popularity[key]):
+                        selected = f'(selected with factor {popularity_factor})' if index==0 else ''
+                        self._info(f'- https://www.imdb.com/title/{film.id}/ votes={film.votes} {selected}')
+
+                    self.tvtropes_imdb_map_popularity[key] = self.tvtropes_imdb_map_popularity[key][0:1]
 
         self._add_to_summary('Films matched times: 1 (popularity heuristic)',
                              len(self._matches_equal_filtering_popularity(1)))
